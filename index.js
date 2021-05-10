66d4d62a8a5e181b6d9c9245f70f7793042bddb4
Victor Adams <kysper50@gmail.com>
2021-05-10 15:27:49 -0500

    added mouseover functionality to the tab / sidebar menu setup more responsiveness added some more filler text for replacement later

--- index.js
+++ index.js
@@ -28,16 +28,15 @@ function sideBarToggle() {
   let tab = document.querySelector(".tab");
   let sideBar = document.querySelector(".sidebar");
   let links = document.querySelectorAll(".link");
-
   //Adds class to open / close navbar
-  tab.addEventListener("click", (event) => {
+  tab.addEventListener("mouseover", (event) => {
     if (tab.classList.contains("clicked")) {
-      tab.textContent = "<<<";
+      tab.textContent = ">>>";
       tab.classList.toggle("clicked");
       sideBar.classList.toggle("open");
       links.forEach((el) => el.classList.toggle("visible"));
     } else {
-      tab.textContent = ">>>";
+      tab.textContent = "<<<";
       tab.classList.toggle("clicked");
       sideBar.classList.toggle("open");
       links.forEach((el) => el.classList.toggle("visible"));
@@ -45,7 +44,7 @@ function sideBarToggle() {
   });
   links.forEach((el) =>
     el.addEventListener("click", (event) => {
-      tab.textContent = "<<<";
+      tab.textContent = ">>>";
       tab.classList.toggle("clicked");
       sideBar.classList.toggle("open");
       links.forEach((el) => el.classList.toggle("visible"));
@@ -59,8 +58,11 @@ function darkMode() {
   let headers = document.querySelectorAll(".headers");
   let content = document.querySelectorAll(".content-body");
   let body = document.querySelector("body");
+    let tab = document.querySelector(".tab");
   toggle.addEventListener("input", (e) => {
     body.classList.toggle("dark-theme");
+    tab.classList.toggle('dark-theme')
+    tab.style.backgroundColor = 'transparent'
     headers.forEach((el) => el.classList.toggle("dark-theme"));
     content.forEach((el) => el.classList.toggle("dark-theme"));
     landingPage.classList.toggle("dark-theme");
